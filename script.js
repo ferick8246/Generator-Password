d2ea5235b6f30e722ed0116ffe75490986772a03
Erick Flore <ferick8246@gmail.com>
2021-03-20 18:46:24 -0500

    added window prompts

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+ var newPasskey = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePssword () {
 
-  passwordText.value = password;
+ 
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Window prompts that set criteria for password
+  
+  var length = parseInt(window.prompt("Password length? (from 8 to 128)"));
+  
+  if (isNaN(length)){
+  
+    alert("Password length must be a number.");
+  
+    return("Bad input. Password not generated. ");}
+  
+   
+  
+  var lower= window.confirm("Do you want lower case letters letters?");
+  
+  var upper= window.confirm("Do you want upper case letters letters?");
+  
+  var numeric= window.confirm("Do you want numbers?");
+  
+  var symbol= window.confirm("Do you want special characters?");
